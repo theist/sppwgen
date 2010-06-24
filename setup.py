3dee74372e07cd5adef2572235b289c7a442c369
Carlos Peñas <cpenas@warp.es>
2010-06-24 10:33:43 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(name='sppwgen',
+      version='0.1',
+      description='Spanish friendly password generator',
+      author='Carlos Peñas San José',
+      author_email='cpenas@warp.es',
+      url='http://cpenas.warp.es',
+      scripts = ['sppwgen']
+      )
